3e51670a227b6243866e731cca5f61fb8447ca7b
marynag <mgrabchak1109@gmail.com>
2025-01-15 14:02:13 +0200

    add scroll to

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -3,12 +3,18 @@ import React from "react";
 import { Button } from "./Button";
 import { Logo } from "./Logo";
 
-export const Header = () => {
+type HeaderProps = {
+  onSubscribeClick: () => void;
+};
+export const Header = ({ onSubscribeClick }: HeaderProps) => {
   return (
     <div className="w-full  flex lg:flex-row lg:justify-between lg:items-start flex-col items-center py-8 gap-8 lg:px-8">
       <Logo />
       <div className="flex flex-col items-center gap-3">
-        <Button text="Subscribe & Receive a discount" onClick={() => {}} />
+        <Button
+          text="Subscribe & Receive a discount"
+          onClick={onSubscribeClick}
+        />
         <p className="text-center w-full max-w-xs 2xl:text-base text-sm">
           <strong>Discounted rates</strong> for the first 100 subscribers
           Subscribe to be issued with the{" "}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,9 @@
-import { Header } from "./components/Header";
-import { CentralBlock } from "./components/CentralBlock";
-import { FeatureBlock } from "./components/FeatureBlock";
-import { additionalFeatures, features } from "../constants/constants";
-import { JoinBlock } from "./components/JoinBlock";
-import { Footer } from "./components/Footer";
-import RunningLine from "./components/RunningLine";
+import Content from "./components/Content";
 
 export default function Home() {
   return (
     <div>
-      <RunningLine />
-      <Header />
-      <CentralBlock />
-      <FeatureBlock
-        title="Why choose DriveSmart to hire your car"
-        features={features}
-      />
-      <FeatureBlock
-        title="Use DriveSmart to build your wealth"
-        features={additionalFeatures}
-      />
-      <JoinBlock />
-      <Footer />
+      <Content />
     </div>
   );
 }
