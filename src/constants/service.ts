3225327d2209713c9ee671860ec7dfef56072fda
marynag <mgrabchak1109@gmail.com>
2025-01-20 11:13:23 +0200

    add instagram link

--- src/constants/service.ts
+++ src/constants/service.ts
@@ -3,6 +3,6 @@ export const CONTACT_US = "contactus@drivesmartuae.com";
 export const X_LINK = "https://x.com/drivesmartuae";
 export const FACEBOOK_LINK = "https://facebook.com";
 export const LINKEDIN_LINK = "https://linkedin.com";
-export const INSTAGRAM_LINK = "https://instagram.com";
+export const INSTAGRAM_LINK = "https://www.instagram.com/drivesmartuae/";
 
 export const COMING_IN = "May 2025";
